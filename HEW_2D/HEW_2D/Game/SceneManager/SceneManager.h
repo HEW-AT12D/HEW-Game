1e789bbe0f5cbe17e5c563fba3469d87f0267acb
Akaneeeeeeeeeee <kaz0815@icloud.com>
2024-12-10 23:32:59 +0900

    オブジェクトマネージャ、シーンマネージャ追加

---
+++ HEW_2D/HEW_2D/Game/SceneManager/SceneManager.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "../../Framework/Precompiled/pch.h"
+#include "../../Framework/Scene/IScene.h"
+
+
+//! TODO:scenemanagerでtitlesceneとresultsceneをインクルードすると、それぞれでインクルードされているobjectmanagerとISceneが衝突してしまう
+//! →scenemanager.cppでのみtitlesceneとresultsceneをインクルードして解決
+
+enum SceneName {
+	TITLE,
+	GAME,
+	RESULT,
+
+	SCENE_MAX
+};
+
+/**
+ * @brief シーン管理クラス
+ * 
+ * タイトル、ゲーム、リザルトの３つだけなので今回はarrayを使用
+ * 初期化でシーンをすべて配列に格納
+ * シーン切り替え関数が必要
+*/
+class SceneManager
+{
+public:
+	SceneManager();
+	~SceneManager();
+
+	void Init(void);		//! 初期化
+	void Update(void);		//! 更新
+	void Draw(void);		//! 描画
+	void Uninit(void);		//! 終了
+
+	void ChangeScene(SceneName scene);	//! シーン切り替え
+
+private:
+	std::array<std::unique_ptr<IScene>, 3> scenes;
+	SceneName scenename;
+};
+
