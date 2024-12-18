2bb5a198ed4eea649ac4b9a6d03e997444c8aca6
Akaneeeeeeeeeee <kaz0815@icloud.com>
2024-12-18 16:17:06 +0900

    調整中

--- HEW_2D/HEW_2D/Game/ObjectManager/ObjectManager.h
+++ HEW_2D/HEW_2D/Game/ObjectManager/ObjectManager.h
@@ -4,9 +4,28 @@
 #include "../Objcet/Player/Player.h"
 #include "../Objcet/SoundGun/SoundGun.h"
 
-enum ObjectName {
+/**
+ * @brief オブジェクト管理タグ
+*/
+enum Tag {
+	BACKGROUND,
+	UI,
 	OBJECT,
 	PLAYER,
+	ENEMY,
+};
+
+
+/**
+ * @brief オブジェクトのmapを管理するために使うstd::pairの二つを紐づける関数
+*/
+struct PairHash {
+	template <typename T1, typename T2>
+	std::size_t operator()(const std::pair<T1, T2>& pair) const {
+		auto hash1 = std::hash<T1>{}(pair.first);  // first のハッシュ値
+		auto hash2 = std::hash<T2>{}(pair.second); // second のハッシュ値
+		return hash1 ^ (hash2 << 1);               // ハッシュ値を組み合わせる
+	}
 };
 
 /**
@@ -27,10 +46,10 @@ public:
 	 * @param _SceneName 
 	*/
 	template <typename T>
-	void AddObject(ObjectName _ObjName)
+	void AddObject(Tag _ObjTag)
 	{
 		// キーとオブジェクトをセットで追加
-		Objects.emplace(_ObjName, std::make_unique<T>(D3d11));
+		Objects.emplace(_ObjTag, std::make_unique<T>(D3d11));
 	}
 
 	/**
@@ -47,7 +66,7 @@ public:
 		//! 　→この場合、std::forwardはTのコンストラクタに引数の型、実引数をそのまま渡すために記述されている
 		 
 		// キーと名前が対応しているかの確認（数が合わない場合、プログラムは実行されずエラーが出る）
-		static_assert(sizeof...(Ts) == sizeof...(Args), "キーの数と追加したいオブジェクトの数が違います");
+		static_assert(sizeof...(Ts) == sizeof...(Args), "キー(タグ)の数と追加したいオブジェクトの数が違います");
 		 
 		//! std::forwardで異なるキーを追加できるようにし、Tsで異なる型のオブジェクトを追加できるようにしている
 		(Objects.emplace(std::forward<Args>(_ObjectsName), std::make_unique<Ts>(D3d11)), ...);		// 全オブジェクト生成時にd3dの参照を渡す
@@ -57,7 +76,7 @@ public:
 	 * @brief オブジェクト削除関数
 	 * @param object 削除対象オブジェクト
 	*/
-	void DeleteObject(ObjectName _ObjName);
+	void DeleteObject(Tag _ObjTag);
 
 	/**
 	 * @brief オブジェクトの数を返す関数
@@ -81,7 +100,11 @@ private:
 	//! オブジェクトのユニークポインタをmapで持つ→オブジェクトマネージャーがオブジェクトを管理する
 	//! 
 	//! (もしかしたら左にあるオブジェクトから順に配列に保持してもいいかも？→その場合もう一度設計が必要)
-	std::unordered_map<ObjectName, std::unique_ptr<GameObject>> Objects;
+	//! 
+	//! オブジェクトの管理はタグ（大まかな分類）と名前（一意のもの）を使う
+	
+	// unordered_mapにpairを使う場合、pairの紐づけないといけないためPairHashをmapの引数に入れる
+	std::unordered_map < std::pair<Tag, std::string>, std::unique_ptr<GameObject>, PairHash > Objects;
 	D3D11& D3d11;
 };
 
