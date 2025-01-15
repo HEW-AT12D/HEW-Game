6ef57d1303577afa2fb7801a672e26ae562f8af0
kairi0825 <0wz3791578c4-8b@ezweb.ne.jp>
2025-01-15 14:12:04 +0900

    Revert "pch調整"
    This reverts commit 90fccfdce773daff56c502eaa656b6ed4c9edc32.

--- HEW_2D/HEW_2D/Framework/Component/RigitBody/RigitBody2D.h
+++ HEW_2D/HEW_2D/Framework/Component/RigitBody/RigitBody2D.h
@@ -3,21 +3,15 @@
 
 /**
  * @brief •¨—‰‰Z
- * 
- * 3D‚É‚à‚Å‚«‚é‚æ‚¤‚É
 */
 class RigitBody2D
 {
 public:
 	RigitBody2D();
-
 	~RigitBody2D();
 
-
-
 private:
-	float m_Angulr;
-	float m_Velocity;
+
 };
 
 RigitBody2D::RigitBody2D()
@@ -26,4 +20,4 @@ RigitBody2D::RigitBody2D()
 
 RigitBody2D::~RigitBody2D()
 {
-}:
+}
