9ec0daafebcfe93d8d1af5aad274f8d2027bc0a3
Akaneeeeeeeeeee <kaz0815@icloud.com>
2025-01-15 11:50:33 +0900

    コンポーネントマネージャ実装中

--- HEW_2D/HEW_2D/Framework/ComponentManager/ComponentManager.cpp
+++ HEW_2D/HEW_2D/Framework/ComponentManager/ComponentManager.cpp
@@ -1,1 +1,40 @@
 #include "ComponentManager.h"
+#include "../Component/Transform/TransformComponent.h"
+
+
+/**
+ * @brief 初期化
+*/
+void ComponentManager::Init(void)
+{
+	// Transformだけを追加
+	auto pt = AddComponent<TransformComponent>();
+	// オブジェクトをアタッチ
+	pt.lock()->SetOwner(m_Owner);
+	// 初期化
+	for (auto& component : m_Components) {
+		component.second->Init();
+	}
+}
+
+/**
+ * @brief 更新 
+*/
+void ComponentManager::Update(void)
+{
+	for (auto& component : m_Components)
+	{
+		component.second->Update();
+	}
+}
+
+/**
+ * @brief 終了 
+*/
+void ComponentManager::Uninit(void)
+{
+	for (auto& component : m_Components)
+	{
+		component.second->Uninit();
+	}
+}
--- HEW_2D/HEW_2D/Framework/ComponentManager/ComponentManager.h
+++ HEW_2D/HEW_2D/Framework/ComponentManager/ComponentManager.h
@@ -4,27 +4,45 @@
 /**
  * @brief コンポーネント管理クラス
  * 
- * コンポーネントどう管理する？
+ * コンポーネントどう管理する？→shared_ptrで管理
+ * 
+ * このクラスで持つべきもの
+ * このクラスを保持する親オブジェクトのポインタ
 */
 class ComponentManager
 {
 public:
-	ComponentManager();
-	~ComponentManager();
+	ComponentManager(GameObject& _Owner) :m_Owner(_Owner) {
+		
+	}
+
+	~ComponentManager() {
+		Uninit();
+	};
+
+	//-----------------------
+	//-----テンプレート関数-----
+	//-----------------------
+	/**
+	 * @brief 
+	 * @tparam T 
+	 * mapにコンポーネントを追加
+	 * 同じコンポーネントが既に存在していたらエラー出したい
+	*/
 
 	/**
 	 * @brief コンポーネント追加関数
 	 * @tparam T コンポーネント
-	 * mapにコンポーネントを追加
-	 * 同じコンポーネントが既に存在していたらエラー出したい
+	 * @param  
+	 * @return コンポーネントのweak_ptr→もし使う場合はlock()をつかってshared_ptrに変換してから使う(参照カウントは増えないので大丈夫)
 	*/
 	template <class T>
-	void AddComponent(void) 
+	std::weak_ptr<T> AddComponent(void) 
 	{
 		// !指定したコンポーネントをmapに追加
 		auto component = std::make_shared<T>();
 		// !既に同じコンポーネントが存在していれば
-		if (m_Components.find(std::type_index(component)) != m_Components.end())
+		if (m_Components.find(std::type_index(typeid(T))) != m_Components.end())
 		{
 			//! エラー
 			throw std::runtime_error("同一コンポーネントが既に存在しています");
@@ -32,18 +50,22 @@ public:
 
 		// !なければ初期化して追加
 		component->Init();
-		m_Components.emplace(std::type_index(typeid(T), component);
+		m_Components.emplace(std::type_index(typeid(T)), component);
 
-		// コンポーネントのポインタを返してもいいかも？
+		// 使う使わないは置いておいて、コンポーネントのポインタを返してもいいかも？
+		// →shared_ptrを返すと変に所有者が増加する可能性があるので、weak_ptrを返す
+		return std::weak_ptr<T>(component);
 	}
 
 	/**
 	 * @brief コンポーネント取得関数
 	 * @tparam T コンポーネントの型
 	 * @return コンポーネントのポインタ
+	 * 
+	 * weak_ptrで取得する関数あってもいいかも
 	*/
 	template <class T>
-	T* GetComponent(T)
+	std::shared_ptr<T> GetComponent(void)
 	{
 		// コンポーネント内を探索
 		for (auto& component : m_Components)
@@ -52,9 +74,11 @@ public:
 			if (T* comp = dynamic_cast<T*>(component.second.get())
 			{
 				//! そのコンポーネントのポインタを返す
-				return comp;
+				return std::shared_ptr<T>(component.second, comp);
 			}
 		}
+		// コンポーネントが存在しなければエラー出力
+		std::cerr<< typeid(T).name() << "を持っていません" <<std::endl;
 	}
 
 	/**
@@ -69,15 +93,19 @@ public:
 		
 	}*/
 
+	//-----------------------
+	//--------通常関数--------
+	//-----------------------
+	void Init(void);	// 所有するオブジェクトのポインタをどう渡すか→コンストラクタ？関数？
+	void Update(void);
+	void Uninit(void);
+
+
 private:
+	//! コンポーネントマネージャを保持するオブジェクトの参照（コンポーネントを持つオブジェクトは必ず存在していなければいけないため）
+	GameObject& m_Owner;
 	//!	コンポーネントの型をキーにしてコンポーネントのポインタを保持しておく→１つのオブジェクトは同じコンポーネントは一種類しか持てない
 	std::unordered_map<std::type_index, std::shared_ptr<IComponent>> m_Components;
 };
 
-ComponentManager::ComponentManager()
-{
-}
 
-ComponentManager::~ComponentManager()
-{
-}
