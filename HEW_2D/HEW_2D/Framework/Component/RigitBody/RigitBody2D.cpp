b195ca954d7dc52014a659b7dba3f21b66bdb01a
kairi0825 <0wz3791578c4-8b@ezweb.ne.jp>
2025-01-20 14:34:55 +0900

    重力追加

--- HEW_2D/HEW_2D/Framework/Component/RigitBody/RigitBody2D.cpp
+++ HEW_2D/HEW_2D/Framework/Component/RigitBody/RigitBody2D.cpp
@@ -1,9 +1,15 @@
 #include"RigitBody2D.h"
 
-int RigitBody2D::NaturalFall(int position)
+/*!
+*	重力を適用させる関数
+*	オブジェクトの高さとジャンプor落下し始めてからの時間、初速度が必要
+*	
+*/
+int RigitBody2D::NaturalFall(int position_y,double time,double velocity)
 {
-	velocity += gravity * steptime;
-	position -= velocity * steptime;
 
-	return position;
+	velocity += gravity * time;		//速度の計算
+	position_y -= velocity * time;	//速度からｙ座標を算出
+
+	return position_y;
 }
