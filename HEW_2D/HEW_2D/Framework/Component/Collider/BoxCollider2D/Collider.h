b0434c5b25f45d829d01ad20d941a6c81f5936f1
Akaneeeeeeeeeee <kaz0815@icloud.com>
2025-01-21 19:33:07 +0900

    行調整、フォルダ分け

--- HEW_2D/HEW_2D/Framework/Component/Collider/BoxCollider2D/Collider.h
+++ HEW_2D/HEW_2D/Framework/Component/Collider/BoxCollider2D/Collider.h
@@ -3,12 +3,12 @@
 
 
 //template <class T, class U>
-bool ColliderPlayer_Ground(Player* , GameObject* );	//Player‚ÆGround‚Ì“–‚½‚è”»’èŠÖ”
+bool ColliderPlayer_Ground(Player*, GameObject*);	//Player‚ÆGround‚Ì“–‚½‚è”»’èŠÖ”
 
 bool ColliderPlayer_Gion(GameObject*, GameObject*);//Player‚Æ‹[‰¹‚Ì“–‚½‚è”»’èŠÖ”
 
 bool ColliderFan_Gion(GameObject*, GameObject*);//îŒ^‚Æ‹[‰¹‚Ì“–‚½‚è”»’èŠÖ”
 
-bool LineIntersectsCircle(float , float , float , float , float , float , float);// ü•ª‚Æ‰~‚ªŒğ·‚µ‚Ä‚¢‚é‚©”»’è‚·‚éŠÖ”
+bool LineIntersectsCircle(float, float, float, float, float, float, float);// ü•ª‚Æ‰~‚ªŒğ·‚µ‚Ä‚¢‚é‚©”»’è‚·‚éŠÖ”
 
 //void DrawFan(GameObject*, bool);
